f783bdc772f8ef55d04b5a76eb8f92a5a05df337
sabrina-jones <sabrina.jones87@gmail.com>
2022-08-15 18:55:46 -0500

    adding for test

--- routes/api.php
+++ routes/api.php
@@ -14,16 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-//Route::resource('users', 'App\Http\Controllers\UserController');
-
 Route::get('/users', 'App\Http\Controllers\UserController@index');
 Route::post('/users/create', 'App\Http\Controllers\UserController@store');
 Route::post('/users/update/{id}', 'App\Http\Controllers\UserController@update');
 
-//Route::post("users/create", 'App\Http\Controllers\UserController\store');
-
-//Route::name('me')->get('users/me', 'User\UserController@me');
+//testing 
